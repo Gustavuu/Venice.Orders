dc23a8dbaf4c36a184c76235e416f6b10a0b4173
Gustavo Irentti <gustavuu@gmail.com>
2025-08-08 15:12:46 -0300

    Testes: Command e Query

---
+++ Venice.Orders.Tests/CriarPedidoCommandHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using Shouldly;
+using Venice.Orders.Application.Features.Pedidos.Commands;
+using Venice.Orders.Application.Interfaces;
+using Venice.Orders.Domain.Entities;
+
+namespace Venice.Orders.Tests
+{
+    public class CriarPedidoCommandHandlerTests
+    {
+        private readonly Mock<IPedidoWriteRepository> _mockWriteRepo;
+        private readonly Mock<IMensageriaService> _mockMessagingService;
+        private readonly CriarPedidoCommandHandler _criarPedidoCommandHandler;
+
+        public CriarPedidoCommandHandlerTests()
+        {
+            // Mock para as dependências externas.
+            _mockWriteRepo = new Mock<IPedidoWriteRepository>();
+            _mockMessagingService = new Mock<IMensageriaService>();
+
+            // Injeta os mocks no handler.
+            _criarPedidoCommandHandler = new CriarPedidoCommandHandler(_mockWriteRepo.Object, _mockMessagingService.Object);
+        }
+
+        [Fact]
+        public async Task Handle_Deve_ChamarRepositorio_E_PublicarMensagem_Quando_Comando_For_Valido()
+        {
+            // Prepara request
+            var request = new CriarPedidoCommand(
+                Guid.NewGuid(),
+                new List<ItemPedidoDto> { new(Guid.NewGuid(), "Teste", 1, 10) });
+
+            // Executa Handle 
+            var pedidoId = await _criarPedidoCommandHandler.Handle(request, CancellationToken.None);
+
+            // Verifica se um ID foi retornado.
+            pedidoId.ShouldNotBe(Guid.Empty); 
+
+            // Verifica se o método Adicionar do repositório foi chamado exatamente uma vez.
+            _mockWriteRepo.Verify(r => r.Adicionar(It.IsAny<Pedido>()), Times.Once);
+
+            // Verifica se o método PublicarMensagem foi chamado exatamente uma vez.
+            _mockMessagingService.Verify(s => s.PublicarMensagem(It.IsAny<object>(), "pedidos-criados"), Times.Once);
+        }
+    }
+}
---
+++ Venice.Orders.Tests/ObterPedidoPorIdQueryHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using Shouldly;
+using Venice.Orders.Application.Features.Pedidos.DTO;
+using Venice.Orders.Application.Features.Pedidos.Queries;
+using Venice.Orders.Application.Interfaces;
+
+namespace Venice.Orders.Tests
+{
+    public class ObterPedidoPorIdQueryHandlerTests
+    {
+        private readonly Mock<IPedidoReadRepository> _mockReadRepo;
+        private readonly Mock<ICacheService> _mockCacheService;
+        private readonly ObterPedidoPorIdQueryHandler _obterPedidoPorIdQueryhandler;
+
+        public ObterPedidoPorIdQueryHandlerTests()
+        {
+            // Mock para as dependências externas
+            _mockReadRepo = new Mock<IPedidoReadRepository>();
+            _mockCacheService = new Mock<ICacheService>();
+
+            // Injeta os mocks no handler
+            _obterPedidoPorIdQueryhandler = new ObterPedidoPorIdQueryHandler(_mockReadRepo.Object, _mockCacheService.Object);
+        }
+
+        [Fact]
+        public async Task Handle_Deve_RetornarPedidoDoCache_E_NaoChamarRepositorio_Quando_PedidoExisteNoCache()
+        {
+            // Prepara variáveis
+            var pedidoId = Guid.NewGuid();
+            var query = new ObterPedidoPorIdQuery(pedidoId);
+            var pedidoEmCache = new PedidoResponseDto(pedidoId, Guid.NewGuid(), DateTime.Now, "Faturado", 100, new());
+
+            // Redis Mock
+            _mockCacheService
+                .Setup(c => c.GetAsync<PedidoResponseDto>($"pedido:{pedidoId}"))
+                .ReturnsAsync(pedidoEmCache);
+
+            // Executa Handle 
+            var resultado = await _obterPedidoPorIdQueryhandler.Handle(query, CancellationToken.None);
+
+            // Verificações
+            resultado.ShouldNotBeNull();
+            resultado.Id.ShouldBe(pedidoId); // Verifica se o ID está correto.
+            resultado.ShouldBe(pedidoEmCache); // Verifica se é o mesmo objeto do cache.
+
+            // Verifica se o método do repositório NUNCA foi chamado, provando que a lógica de cache funcionou.
+            _mockReadRepo.Verify(r => r.ObterPorIdAsync(It.IsAny<Guid>()), Times.Never);
+        }
+    }
+}
